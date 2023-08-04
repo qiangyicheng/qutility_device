3928b6cb0616453cd82ace12a83d081eca2696cc
Yicheng Qiang <qiangyicheng@icloud.com>
2023-08-04 14:14:51 +0200

    minor fixes

--- include/qutility_device/workspace/workspace.h
+++ include/qutility_device/workspace/workspace.h
@@ -58,8 +58,8 @@ namespace qutility
 
                 operator device_pointer_t() { return (device_pointer_t)pointer_; }
                 operator device_const_pointer_t() const { return (device_pointer_t)pointer_; }
-                device_pointer_t pointer() -> device_pointer_t { return (device_pointer_t)pointer_; }
-                device_const_pointer_t pointer() const -> device_pointer_t { return (device_const_pointer_t)pointer_; }
+                auto pointer() -> device_pointer_t { return (device_pointer_t)pointer_; }
+                auto pointer() const -> device_const_pointer_t { return (device_const_pointer_t)pointer_; }
             };
 
         }
